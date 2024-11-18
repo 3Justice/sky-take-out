d8fa50a3ca1089af80528b4a3d4249a1d773bdf2
3Justice <1475306447@qq.com>
2024-11-18 23:17:40 +0800

    启用禁用员工账号业务代码

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -115,4 +115,24 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     }
 
+    /**
+     * 启用禁用员工账号
+     * @param status
+     * @param id
+     */
+    public void startOrStop(Integer status,Long id){
+        // update employee set status = ? where id = ?
+//        Employee employee = new Employee();
+//        employee.setStatus(status);
+//        employee.setId(id);
+//        以上方法不常用，下列为便利的方法
+
+        Employee employee = Employee.builder()
+                .status(status)
+                .id(id)
+                .build();
+        employeeMapper.update(employee);
+
+    }
+
 }
